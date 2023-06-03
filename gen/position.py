046d9547e740af060495fd6684319896e2ad5dd0
SquareSheep Studios <dev@dansmith.tech>
2023-06-04 00:43:56 +0100

    Begin implement position retrieve for new textbox and refactor (but crashes 80% of the time)

--- gen/position.py
+++ gen/position.py
@@ -1,6 +1,6 @@
 import numpy as np
 import random
-from collections import namedtuple
+from PIL import Image, ImageDraw
 
 
 class Rect:
@@ -25,78 +25,26 @@ def get_minkowski_bounds(box, collider):
                 collider.width + box.width,
                 collider.height + box.height)
 
-# not needed imo
-def get_image_bound_colliders(new_text_box, img_size):
-    """
-    Gets the boxes where a new text box's anchors cannot be placed.
 
-    :param new_text_box: a Rect of the text to be placed in the image
-    :param img_size: the resolution (height, width) of the image the text is being placed in
-    :return: the Rects of the colliders
-    """
-
-    pass
-
-# not needed imo
-def get_valid_bounds(existing_text_boxes, img_size):
-    """
-    Divide and conquer to find all boxes outside the colliders:
-    https://stackoverflow.com/questions/60509252/how-do-i-get-the-rectangles-which-would-fill-out-a-space-excluding-some-other
-
-    :param existing_text_boxes: a list of Rects of the existing text boxes
-    :param img_size: the resolution (height, width) of the image the text is being placed in
-    :return: a list of Rects of containing valid placement positions
-    """
-
-    pass
-
-def minkowski_overlap_fix(colliders):
-    """
-    Fixes overlapping boxes, somehow.
-    !!will not work by simply reducing size on axis - think about corner overlaps. will have to create additional rects probably?
-    :param colliders: a list of input minkowski'd colliders that may overlap
-    :return: a list of colliders that no longer overlap but cover the same area
-    """
-
-    return colliders
-
-
-def get_text_position(new_text_box, existing_text_boxes, img_size):
-    """
-    Gets the position to place a text box in.
-
-    :param new_text_box: a tuple containing (x, y, width, height) of the text to be placed in the image
-    :param existing_text_boxes: a list of tuples containing (x, y, width, height) of the existing text boxes
-    :param img_size: the resolution (height, width) of the image the text is being placed in
-    :return: the position (y, x) of the text box
-    """
-
-    # colliders = np.empty((len(existing_text_boxes) + 2))
+def get_collision_bounds(new_text_box, existing_text_boxes, img_size):
+    colliders = []
 
     # get minkowski bounds for existing text boxes and image size store in colliders
     # overlap may occur after minkowski. CAUSES ISSUES IN DIVCONQ
     # no anchor conversion needed, pillow now set to use topleft anchor. is only ever a couple of pixels out on the x axis, negligible therefore ignoring
-    colliders = []
-    for box in existing_text_boxes:
-        colliders.append(get_minkowski_bounds(new_text_box, Rect(box.x, box.y, box.width, box.height)))
+    for i, box in enumerate(existing_text_boxes):
+        colliders.append(get_minkowski_bounds(new_text_box, box))
     # add rightmost and bottommost minkowski bounds
     colliders.append(Rect(img_size.width - new_text_box.width, 0, new_text_box.width, img_size.height))
-    colliders.append(Rect(0, img_size.height - new_text_box.height, img_size.width, new_text_box.height))
+    colliders.append(Rect(0, img_size.height - new_text_box.height, img_size.width,
+                          new_text_box.height))
 
-    colliders = minkowski_overlap_fix(colliders)
+    return colliders
 
-    # divide and conquer to find all possible valid points
-    valid_rects = split_rectangles(Rect(0, 0, img_size.width, img_size.height), colliders)
-    # pick a random valid Rect, and a random position inside that Rect, and return
-    # !! dont forget to check for edgecase where no valid positions exist
-    chosen = random.choice(valid_rects)
-    return np.random.randint(chosen.y, (chosen.y + chosen.height) + 1), np.random.randint(chosen.x, (chosen.x + chosen.width) + 1)
-
-# div and conq crap from hereon out
-# !! must fix for overlap possibility !! either here or in minkowski (probably here)
 
 def intersects(b, r):
     return b.x < r.x + r.width and b.x + b.width > r.x and b.y < r.y + r.height and b.y + b.height > r.y
+
 
 # custom min thing to adjust for relative anchor and not absolute
 def custom_minW(b, r):
@@ -104,12 +52,14 @@ def custom_minW(b, r):
         return b.width
     else:
         return r.width
-    
+
+
 def custom_minH(b, r):
     if (min(b.x + b.height, r.x + r.height) == b.x + b.height):
         return b.height
     else:
         return r.height
+
 
 def clip_rect(b, r):
     return Rect(
@@ -117,11 +67,12 @@ def clip_rect(b, r):
         custom_minW(b, r), custom_minH(b, r)
     )
 
+
 def clip_rects(b, rects):
     return [clip_rect(b, r) for r in rects if intersects(b, r)]
 
-# entrypoint
-def split_rectangles(b, rects):
+
+def get_valid_bounds(rects, b):
     if b.x >= b.x + b.width or b.y >= b.y + b.height:
         pass
     elif not rects:
@@ -131,20 +82,90 @@ def split_rectangles(b, rects):
         # change this if deterministic behaviour is required
         pivot = random.choice(rects)
 
-        above = Rect(b.x,     b.y,     b.x + b.width,     pivot.y)
-        left  = Rect(b.x,     pivot.y, pivot.x, pivot.height)
-        right = Rect(pivot.x + pivot.width, pivot.y, b.width - (pivot.x + pivot.width),     pivot.height)
-        below = Rect(b.x,     pivot.y + pivot.height, b.x + b.width,     b.height - (pivot.y + pivot.height))
+        above = Rect(b.x, b.y, b.x + b.width, pivot.y)
+        left = Rect(b.x, pivot.y, pivot.x, pivot.height)
+        right = Rect(pivot.x + pivot.width, pivot.y, b.width - (pivot.x + pivot.width), pivot.height)
+        below = Rect(b.x, pivot.y + pivot.height, b.x + b.width, b.height - (pivot.y + pivot.height))
 
-        yield from split_rectangles(above, clip_rects(above, rects))
-        yield from split_rectangles(left,  clip_rects(left,  rects))
-        yield from split_rectangles(right, clip_rects(right, rects))
-        yield from split_rectangles(below, clip_rects(below, rects))
+        yield from get_valid_bounds(clip_rects(above, rects), above)
+        yield from get_valid_bounds(clip_rects(left, rects), left)
+        yield from get_valid_bounds(clip_rects(right, rects), right)
+        yield from get_valid_bounds(clip_rects(below, rects), below)
+
+
+def draw_boxes(sections, colliders, img_size, new_text_box=None):
+    img = Image.new("RGB", img_size)
+
+    for section in sections:
+        new_img = ImageDraw.Draw(img)
+        new_img.rectangle(((section.x, section.y), (section.x + section.width, section.y + section.height)),
+                          fill="white", outline="yellow", width=3)
+
+    for collider in colliders:
+        new_img = ImageDraw.Draw(img)
+        new_img.rectangle(((collider.x, collider.y), (collider.x + collider.width, collider.y + collider.height)),
+                          fill="black")
+
+    if new_text_box:
+        new_img = ImageDraw.Draw(img)
+        new_img.rectangle(((new_text_box.x, new_text_box.y),
+                           (new_text_box.x + new_text_box.width, new_text_box.y + new_text_box.height)), fill="blue")
+
+    img.show()
+
+
+def get_text_position(new_text_box_pos, existing_text_boxes_pos, img_size, draw=False):
+    """
+    Gets the position to place a text box in.
+
+    :param new_text_box_pos: a tuple containing (x, y, width, height) of the text to be placed in the image
+    :param existing_text_boxes_pos: a list of tuples containing (x, y, width, height) of the existing text boxes
+    :param img_size: the resolution (width, height) of the image the text is being placed in
+    :param draw: shows the box placement as an image
+    :return: the position (y, x) of the text box
+    """
+
+    new_text_box = Rect(-1, -1, new_text_box_pos[0], new_text_box_pos[1])
+    existing_text_boxes = [Rect(box[0], box[1], box[2], box[3]) for box in existing_text_boxes_pos]
+    img_bounds = Rect(0, 0, img_size[0], img_size[1])
+
+    colliders = get_collision_bounds(new_text_box, existing_text_boxes, img_bounds)
+
+    for c in colliders:
+        print(c.x, c.y, c.width, c.height)
+    print(img_bounds.x, img_bounds.y, img_bounds.width, img_bounds.height, end="\n----------------\n")
+
+    for thing in get_valid_bounds(colliders, img_bounds):
+        print(thing.x, thing.y, thing.width, thing.height)
+
+    valid_rects = list(get_valid_bounds([colliders[2]], img_bounds))
+
+    if len(valid_rects) > 0:
+        chosen = random.choice(valid_rects)
+        new_text_box.x = np.random.randint(chosen.x, (chosen.x + chosen.width) + 1)
+        new_text_box.y = np.random.randint(chosen.y, (chosen.y + chosen.height) + 1)
+
+        if draw:
+            draw_boxes(valid_rects, colliders, img_size, new_text_box=new_text_box)
+    elif draw:
+        draw_boxes(valid_rects, colliders, img_size)
 
 
 if __name__ == "__main__":
-    # get_text_position((56, 13, 100, 50), [], (1080, 1920))
-    existing = [Rect(100, 200, 300, 400), Rect(400, 200, 700, 200)]
+    existing = [
+        Rect(50, 150, 350, 450),
+        Rect(1870, 0, 50, 1080),
+        Rect(0, 1030, 1920, 50),
+    ]
     bounding = Rect(0, 0, 1920, 1080)
-    for things in split_rectangles(bounding, existing):
+
+    for e in existing:
+        print(e.x, e.y, e.width, e.height)
+    print(bounding.x, bounding.y, bounding.width, bounding.height, end="\n----------------\n")
+
+    for things in get_valid_bounds(existing, bounding):
         print(things.x, things.y, things.width, things.height)
+
+    print("\ncompared to...\n")
+
+    get_text_position((50, 50), [(100, 200, 300, 400)], (1920, 1080), draw=True)
