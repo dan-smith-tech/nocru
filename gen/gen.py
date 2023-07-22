c882e5fb9024be18e7dcfa289022970c20d97013
Dan Smith <dev@dansmith.tech>
2023-07-22 01:34:01 +0100

    Fix cutter overlap (plus major refactor)

--- gen/gen.py
+++ gen/gen.py
@@ -1,5 +1,5 @@
-from perlin_numpy import generate_perlin_noise_2d
 from essential_generators import DocumentGenerator
+from perlin_numpy import generate_perlin_noise_2d
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 import random
@@ -7,11 +7,15 @@ import json
 import re
 import os
 
-from pos import TextBox
 from pos import get_position
+from pos import TextBox
 
 
 class NpTypeEncoder(json.JSONEncoder):
+    """
+    Encoder override to convert numpy types to standard Python types for JSON serialization.
+    """
+
     def default(self, obj):
         if isinstance(obj, np.integer):
             return int(obj)
@@ -23,6 +27,12 @@ class NpTypeEncoder(json.JSONEncoder):
 
 
 def get_sentence():
+    """
+    Randomly generates a nonsense sentence.
+
+    :return: String of the random sentence
+    """
+
     gen = DocumentGenerator()
 
     sentence = gen.sentence()
@@ -33,80 +43,105 @@ def get_sentence():
     return sentence
 
 
-def generate_image(img_size, noise_scale=(27, 48)):
+def get_font(sentence, draw, img_size):
     """
+    Randomly selects a font from the 'gen/fonts' directory.
+
+    :param sentence: String representing the sentence to be displayed
+    :param draw: ImageDraw (mutated) that holds the current state of the image being generated
     :param img_size: (width, height) of the image
-    :param noise_scale: scale of perlin noise pattern (smaller is more zoomed in, must be multiples of respective axes)
-    :return: the generated image
+    :return: ImageFont.FreeTypeFont of the selected font
     """
+
+    font = ImageFont.FreeTypeFont("fonts/" + random.choice(os.listdir("fonts/")), np.random.randint(50, 100))
+
+    # TODO: what exactly does this do?
+    while (draw.textbbox((0, 0), sentence, font=font, anchor="lt")[2] > img_size[0] or
+           draw.textbbox((0, 0), sentence, font=font, anchor="lt")[3] > img_size[1]):
+        font = ImageFont.FreeTypeFont("fonts/" + random.choice(os.listdir("fonts/")), np.random.randint(50, 100))
+
+    return font
+
+
+def create_textbox(existing_boxes, draw, img_size):
+    """
+    Generates a TextBox to be placed on an image.
+
+    :param existing_boxes: [TextBox] of the current boxes placed on the image
+    :param draw: ImageDraw (mutated) that holds the current state of the image being generated
+    :param img_size: (width, height) of the image
+    :return: TextBox of the new text placed on the image
+    """
+
+    sentence = get_sentence()
+    font = get_font(sentence, draw, img_size)
+
+    left, top, width, height = draw.textbbox((0, 0), sentence, font=font, anchor="lt")
+    color, stroke_color = random.sample([0, 255], 2)
+    stroke_width = random.choice([2, 6])
+    new_box = TextBox(0, 0, width, height, sentence, font, color, stroke_width, stroke_color)
+
+    x_pos, y_pos = get_position(new_box, existing_boxes, (img_size[0], img_size[1]))
+    new_box.x = x_pos
+    new_box.y = y_pos
+
+    # 50% chance to add an overlap box
+    if np.random.randint(0, 2):
+        new_box.cutter_x = new_box.x
+        new_box.cutter_y = new_box.y + new_box.height - np.random.randint(0, 5)
+        new_box.cutter_width = new_box.width
+        new_box.cutter_height = np.random.randint(10, 50)
+        new_box.cutter_color = random.choice([0, 255])
+
+    return new_box
+
+
+def generate_image():
+    """
+    Generates a random image containing random text and a perlin noise backdrop, labelled.
+
+    :return: (Image, [TextBox]) of the generated image and respective label
+    """
+
+    # hardcoded image properties that match scaling of other features (e.g., font size)
+    img_size = (1920, 1080)
+    noise_scale = (27, 48)
 
     noise = (generate_perlin_noise_2d((img_size[1], img_size[0]), noise_scale) * 255).astype(np.uint8)
     img = Image.fromarray(noise)
     draw = ImageDraw.Draw(img)
-    boxes = []
     text_boxes = []
 
-    num_sentences = np.random.randint(1, 8)
-    cur_sentences = 0
-    while cur_sentences < num_sentences:
-        sentence = get_sentence()
-        font = ImageFont.FreeTypeFont(
-            "fonts/" + random.choice(os.listdir("fonts/")), np.random.randint(50, 100)
-        )
-        while (draw.textbbox((0, 0), sentence, font=font, anchor="lt")[2] > img_size[0] or
-               draw.textbbox((0, 0), sentence, font=font, anchor="lt")[3] > img_size[1]):
-            font = ImageFont.FreeTypeFont("fonts/" + random.choice(os.listdir("fonts/")), np.random.randint(50, 100))
+    for i in range(random.randrange(8)):
+        new_box = create_textbox(text_boxes, draw, img_size)
 
-        left, top, width, height = draw.textbbox((0, 0), sentence, font=font, anchor="lt")
-        color, stroke_color = random.sample([0, 255], 2)
-        stroke_width = random.choice([2, 6])
-        new_box = TextBox(0, 0, width, height, sentence, font, color, stroke_width, stroke_color)
-
-        x_pos, y_pos = get_position(new_box, boxes, (img_size[0], img_size[1]))
-        new_box.x = x_pos
-        new_box.y = y_pos
-
-        if x_pos > -1 and y_pos > -1:
-            draw.text(
-                (x_pos, y_pos),
-                new_box.text,
-                font=new_box.font,
-                fill=new_box.color,
-                stroke_width=new_box.stroke_width,
-                stroke_fill=new_box.stroke_color,
-            )
-            if np.random.randint(0, 2):
-                offset = np.random.randint(0, 20)
-                cutter = TextBox(new_box.x - offset, (new_box.y + new_box.height) - np.random.randint(0, 5),
-                                 new_box.width + offset + np.random.randint(0, 20), np.random.randint(10, 50),
-                                 None, None, random.choice([0, 255]), 0, None)
-                draw.rectangle((cutter.x, cutter.y, cutter.x + cutter.width, cutter.y + cutter.height),
-                               fill=cutter.color, outline=None, width=1)
-                boxes.append(cutter)
-                new_box.cutter_x = cutter.x
-                new_box.cutter_y = cutter.y
-                new_box.cutter_width = cutter.width
-                new_box.cutter_height = cutter.height
-                new_box.cutter_color = cutter.color
-            boxes.append(new_box)
+        # if the text fits on the image somewhere, place it
+        if new_box.x > -1 and new_box.y > -1:
+            draw.text((new_box.x, new_box.y), new_box.text, font=new_box.font, fill=new_box.color,
+                      stroke_width=new_box.stroke_width, stroke_fill=new_box.stroke_color)
+            # change FreeTypeFont to (name, weight) of font
             new_box.font = new_box.font.getname()
-            text_boxes.append(new_box)
 
-        cur_sentences += 1
+            # if there is a cutter associated with this text box, place it
+            if new_box.cutter_x:
+                draw.rectangle((new_box.cutter_x, new_box.cutter_y, new_box.cutter_x + new_box.cutter_width,
+                                new_box.cutter_y + new_box.cutter_height),
+                               fill=new_box.cutter_color, outline=None, width=1)
+
+            text_boxes.append(new_box)
 
     return img, text_boxes
 
 
-def generate_dataset(num_images, directory, img_size):
+def generate_dataset(size, start=0, directory="../dataset/"):
     """
-    :param num_images: Integer quantity of images to generate
-    :param directory: relative location to save images in (include trailing slash)
-    :param img_size: (width, height) of the image
-    :return: None
+    :param size: Integer quantity of images to generate
+    :param start: Integer index to begin generating at
+    :param directory: String of relative location to save images to (including trailing slash)
     """
 
-    for i in range(num_images):
-        new_image, text_boxes = generate_image(img_size)
+    for i in range(start, start + size):
+        new_image, text_boxes = generate_image()
         new_image.save(directory + str(i) + ".png")
 
         label = {
@@ -119,4 +154,14 @@ def generate_dataset(num_images, directory, img_size):
 
 
 if __name__ == "__main__":
-    generate_dataset(1, "../out/", (1920, 1080))
+    print("Size of dataset: ")
+    size = int(input())
+    
+    print("Start index: ")
+    start = int(input())
+
+    print("Dataset generating...")
+
+    generate_dataset(size, start=start)
+
+    print("Dataset complete.")
--- gen/pos.py
+++ gen/pos.py
@@ -1,9 +1,10 @@
 import numpy as np
+import copy
 
 
 class TextBox(object):
-    def __init__(self, x, y, width, height, text, font, color, stroke_width, stroke_color, cutter_x=None, cutter_y=None,
-                 cutter_width=None, cutter_height=None, cutter_color=None):
+    def __init__(self, x, y, width, height, text, font, color, stroke_width, stroke_color,
+                 cutter_x=None, cutter_y=None, cutter_width=None, cutter_height=None, cutter_color=None):
         self.x = x
         self.y = y
         self.width = width
@@ -22,29 +23,36 @@ class TextBox(object):
 
 def get_minkowski_bounds(new_box, existing_box, img_size):
     """
-    :param new_box: TextBox representing the box to be added to the image
-    :param existing_box: TextBox representing a box currently in an image
+    :param new_box: TextBox of the box to be added to the image
+    :param existing_box: TextBox of a box currently in an image
     :param img_size: (width, height) of the image
-    :return: TextBox representing collider with a bounding area
+    :return: TextBox of the collider including the bounding area
     """
 
-    rect = TextBox(existing_box.x - new_box.width,
-                   existing_box.y - new_box.height,
-                   existing_box.width + new_box.width,
-                   existing_box.height + new_box.height,
-                   existing_box.text, existing_box.font,
-                   existing_box.color, existing_box.stroke_width, existing_box.stroke_color)
+    # if there is a cutter associated with the text box to be placed, adjust the bounds accordingly
+    cutter_offset = 0
+    if new_box.cutter_x:
+        cutter_offset = new_box.y + new_box.height - new_box.cutter_y + new_box.cutter_height
 
+    rect = TextBox(existing_box.x - new_box.width,
+                   existing_box.y - new_box.height - cutter_offset,
+                   existing_box.width + new_box.width,
+                   existing_box.height + new_box.height + cutter_offset,
+                   existing_box.text,
+                   existing_box.font,
+                   existing_box.color,
+                   existing_box.stroke_width,
+                   existing_box.stroke_color)
+
+    # clamp bounds
     if rect.x < 0:
         rect.width += rect.x
         rect.x = 0
     if rect.y < 0:
         rect.height += rect.y
         rect.y = 0
-    if rect.width > img_size[0]:
-        rect.width = img_size[0]
-    if rect.height > img_size[1]:
-        rect.height = img_size[1]
+    rect.width = min(rect.width, img_size[0])
+    rect.height = min(rect.height, img_size[1])
 
     return rect
 
@@ -57,18 +65,32 @@ def get_position(new_box, existing_boxes, img_size):
     :return: (x, y) position to place the textbox on the image
     """
 
+    # each point represents whether `new_box` can be placed there or not
     img = np.zeros((img_size[1], img_size[0]))
 
-    existing_boxes.append(TextBox(0, img_size[1], img_size[0], new_box.height, None, None, None, None, None))
-    existing_boxes.append(TextBox(img_size[0], 0, new_box.width, img_size[1], None, None, None, None, None))
-    for existing_box in existing_boxes:
+    boxes = copy.copy(existing_boxes)
+
+    # pad image size
+    boxes.append(TextBox(0, img_size[1], img_size[0], new_box.height, None, None, None, None, None))
+    boxes.append(TextBox(img_size[0], 0, new_box.width, img_size[1], None, None, None, None, None))
+
+    for existing_box in boxes:
         collider = get_minkowski_bounds(new_box, existing_box, img_size)
         img[collider.y:collider.y + collider.height, collider.x:collider.x + collider.width] = 1
 
+        # if there is a cutter associated with the current existing text box, remove the relevant points from options
+        if collider.cutter_x:
+            cutter = TextBox(existing_box.cutter_x, existing_box.cutter_y, existing_box.cutter_width,
+                             existing_box.cutter_height, None, None, None, None, None)
+            img[cutter.y:cutter.y + cutter.height, cutter.x:cutter.x + cutter.width] = 1
+
+    # find possible coordinates
     y, x = np.where(img == 0)
 
+    # check if there are valid positions
     if len(x) == 0 or len(y) == 0:
         return -1, -1
 
+    # select a random coordinate pair
     i = np.random.randint(len(x))
-    return [x[i], y[i]]
+    return x[i], y[i]
